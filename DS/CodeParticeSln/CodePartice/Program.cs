ce6e523d93f146e05f57385e0ef89fc6cecd7103
shuvrangshu <shuvrangshu2010@gmail.com>
2019-08-18 12:34:21 +0530

    init commit

---
+++ DS/CodeParticeSln/CodePartice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePartice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task.Factory.StartNew(methodOne);
+            Task.Factory.StartNew(methodTwo);
+
+            Console.WriteLine("Enter Name...");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your Name : " + name);
+            Console.ReadLine();
+        }
+
+        static void methodOne()
+        {
+            Task.Delay(10000).Wait();
+            Console.WriteLine("Download File 1.....");
+        }
+
+        static void methodTwo()
+        {
+            Task.Delay(10000).Wait();
+            Console.WriteLine("Download File 1.....");
+        }
+    }
+}
